0315cf179171a1a72024ef1429e27d78e4add1d1
LENOVO <sumeyra.erkol.tr.@gmail.com>
2023-01-30 21:57:47 +0300

    ilk task yapıldı

--- Hellooo.java
+++ Hellooo.java
@@ -1,5 +1,7 @@
 public class Hellooo {
     public static void main(String[] args) {
         System.out.println("Grup 19 bi başka bee !!!!!!");
+        System.out.println("Bu grup çok işler yapacak:))");
+
     }
 }
