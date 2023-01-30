0b56a0febef572b727af57086146dd9895789023
Sureya <sureyyaserimm@gmail.comm>
2023-01-30 10:54:39 -0800

    bitirdimmmm bir şeylerrrrr

--- Hellooo.java
+++ Hellooo.java
@@ -1,5 +1,8 @@
 public class Hellooo {
     public static void main(String[] args) {
         System.out.println("Grup 19 bi başka bee !!!!!!");
+
+        System.out.println("Bensiz olsaydı güzel olmazdı :)");
+
     }
 }
---
+++ Sureya.java
@@ -0,0 +1,8 @@
+public class Sureya {
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
